b06148064a518c2e3b38235a65497dec4120c044
SuperB3333 <superb3333ghg@gmail.com>
2026-04-17 07:23:47 +0200

    Tried to fix planets not loading into the database

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ use std::io::Write;
 use crate::data::enums::{PlanetType, ORES};
 use crate::data::game_desc::GameDesc;
 use crate::worldgen::galaxy_gen::create_galaxy;
-fn insert_seed(scopy: &mut CopyInWriter, pcopy: &mut CopyInWriter, seed: i32, star_count: usize, resource_multiplier: f32) -> Result<(), Box<dyn std::error::Error>> {
+
+use std::fs::File;
+
+fn insert_seed(scopy: &mut File, pcopy: &mut File, seed: i32, star_count: usize, resource_multiplier: f32) -> Result<(), Box<dyn std::error::Error>> {
 
     let mut game_desc: GameDesc = GameDesc::default();
     game_desc.seed = seed;
@@ -21,7 +24,7 @@ fn insert_seed(scopy: &mut CopyInWriter, pcopy: &mut CopyInWriter, seed: i32, st
 
         write!(scopy, "{},", star_id)?;
         write!(scopy, "{},", seed)?;
-        write!(scopy, "{},", star.position.magnitude())?; //todo change to magnitude_sq and change rule to account for that
+        write!(scopy, "{},", star.position.magnitude())?;
         write!(scopy, "{},", star.index)?;
         write!(scopy, "{},", star.get_luminosity())?;
         write!(scopy, "{},", star.get_dyson_radius())?;
@@ -90,13 +93,14 @@ fn insert_seed(scopy: &mut CopyInWriter, pcopy: &mut CopyInWriter, seed: i32, st
 }
 
 const START_SEED: i32 = 0;
-const END_SEED: i32 = 1000;
+const END_SEED: i32 = 1;
 const STAR_COUNT: usize = 64;
 const REC_MULTIPLIER: f32 = 1.0;
 
 
 const COPY_PLANET: &str = "COPY planets(star_id, index, water_item, gas_giant, sun_distance, inside_ds, satellites, temperature, theme_id, gas_h, gas_d, gas_i, tidal_lock, min_iron, max_iron, estimate_iron, min_copper, max_copper, estimate_copper, min_silicium, max_silicium, estimate_silicium, min_titanium, max_titanium, estimate_titanium, min_stone, max_stone, estimate_stone, min_coal, max_coal, estimate_coal, min_oil, max_oil, estimate_oil, min_fireice, max_fireice, estimate_fireice, min_diamond, max_diamond, estimate_diamond, min_fractal, max_fractal, estimate_fractal, min_crysrub, max_crysrub, estimate_crysrub, min_grat, max_grat, estimate_grat, min_bamboo, max_bamboo, estimate_bamboo, min_mag, max_mag, estimate_mag) FROM STDIN WITH (FORMAT CSV)";
 const COPY_STAR: &str = "COPY stars(id, seed, start_dist, star_index, luminosity, dyson_radius, type, spectr, ore_iron, ore_copper, ore_silicium, ore_titanium, ore_stone, ore_coal, ore_oil, ore_fireice, ore_diamond, ore_fractal, ore_crysrub, ore_grat, ore_bamboo, ore_mag) FROM STDIN WITH (FORMAT CSV)";
+
 fn main() {
     let mut star_client = Client::connect("postgres://postgres:rootpassword@localhost:5432/dsp?sslmode=disable", NoTls).unwrap();
     let mut planet_client = Client::connect("postgres://postgres:rootpassword@localhost:5432/dsp?sslmode=disable", NoTls).unwrap();
@@ -104,8 +108,11 @@ fn main() {
     let mut scpy = star_client.copy_in(COPY_STAR).unwrap();
     let mut pcpy = planet_client.copy_in(COPY_PLANET).unwrap();
 
+    let mut tcpy1 = File::create("stars.csv").unwrap();
+    let mut tcpy2 = File::create("planets.csv").unwrap();
+
     for seed in START_SEED..END_SEED {
-        insert_seed(&mut scpy, &mut pcpy, seed, STAR_COUNT, REC_MULTIPLIER).expect("insert_seed failed");
+        insert_seed(&mut tcpy1, &mut tcpy2, seed, STAR_COUNT, REC_MULTIPLIER).expect("insert_seed failed");
     }
     scpy.finish().unwrap();
     pcpy.finish().unwrap();
