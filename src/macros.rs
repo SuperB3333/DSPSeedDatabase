546b71afb881e5d15ab7e99e9644e97c96bc9e6a
SuperB3333 <superb3333ghg@gmail.com>
2026-06-09 15:57:57 +0200

    Finally added env var configurations

---
+++ src/macros.rs
@@ -0,0 +1,23 @@
+
+
+macro_rules! env_int {
+    ($var:expr) => {
+        match std::env::var($var) {
+            Ok(val) => val.parse(),
+            Err(err) => Err(err)
+        }
+    };
+    ($var:expr, $default:expr) => {
+        std::env::var($var).map(|e| e.parse::<i32>().unwrap_or($default)).unwrap_or($default)
+    };
+}
+pub(crate) use env_int;
+macro_rules! env_str {
+    ($var:expr) => {
+        std::env::var($var)
+    };
+    ($var:expr, $default:expr) => {
+        std::env::var($var).unwrap_or($default.to_string())
+    }
+}
+pub(crate) use env_str;
