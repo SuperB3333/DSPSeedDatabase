5cfdabbbfaba321ebb49b4c40a3462ff2d3489f2
SuperB3333 <superb3333ghg@gmail.com>
2026-06-11 13:42:56 +0200

    Implemented creating checkpoints

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,16 @@ use std::{
     ops::Range,
     thread
 };
+use std::fs::OpenOptions;
 use std::io::stdout;
 use std::sync::atomic::AtomicI32;
+use std::sync::atomic::Ordering::{Relaxed};
 use crossterm::ExecutableCommand;
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use generate_csv::gen_formatted;
 use macros::env_int;
 use misc::{split_chunks, COPY_PLANET, COPY_STAR, get_db_str};
+use crate::macros::env_str;
 use crate::metrics::write_metrics;
 
 const STAR_COUNT: usize = 64;
@@ -26,10 +29,13 @@ const REC_MULTIPLIER: f32 = 1.0;
 
 static COMMITTED_SEEDS: AtomicI32 = AtomicI32::new(0);
 
-fn worker_per_thread(seeds: Range<i32>, send: Sender<(String, String)>) {
+static PROGRESS_WORKERS: [AtomicI32; 32] = [const { AtomicI32::new(0) }; 32];
+
+fn worker_thread(seeds: Range<i32>, send: Sender<(String, String)>, id: usize) {
     for seed in seeds {
         let entry = gen_formatted(seed, STAR_COUNT, REC_MULTIPLIER).expect("gen_formatted failed");
         send.send(entry).unwrap();
+        PROGRESS_WORKERS[id].fetch_add(1, Relaxed);
 
     }
 }
@@ -62,17 +68,21 @@ fn commit_thread(rec: Receiver<(String, String)>, config: (String, i32)) {
 }
 fn main() {
     // Retrieve Config
-    let start_seed = env_int!("START_SEED", 0);
-    let end_seed = env_int!("END_SEED", 10_000);
+    let start_seed   = env_int!("START_SEED", 0);
+    let end_seed     = env_int!("END_SEED", 10_000);
     let worker_count = env_int!("WORKER_THREADS", 8);
     let writer_count = env_int!("WRITER_THREADS", 4);
     let commit_count = env_int!("COMMIT_COUNT", 1000);
     let channel_size = env_int!("CHANNEL_SIZE", 1000);
 
+    let checkpoint_file = env_str!("CHECKPOINT_FILE", "checkpoints.txt");
+
     let conf = (get_db_str(), commit_count);
 
     assert!(start_seed < end_seed);
     assert!(worker_count < end_seed);
+    assert!(worker_count < 32);
+
 
     let start = Instant::now();
 
@@ -84,10 +94,11 @@ fn main() {
     let mut work_handles = vec![];
     let mut commit_handles = vec![];
     // Launch worker threads
-    for work in workloads {
+    for (id, work) in workloads.iter().enumerate() {
         let thread_sender = entry_sender.clone();
+        let thread_work = work.clone();
         work_handles.push(thread::spawn(move || {
-            worker_per_thread(work, thread_sender);
+            worker_thread(thread_work, thread_sender, id);
         }))
     }
     // Launch database threads
@@ -103,6 +114,22 @@ fn main() {
     stdout.execute(crossterm::cursor::Hide).unwrap();
 
     loop {
+        let max_buffer = channel_size + commit_count * writer_count;
+        let mut cfile = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(&checkpoint_file)
+            .unwrap();
+        PROGRESS_WORKERS
+            .iter()
+            .map(|i| i.load(Relaxed) - max_buffer)
+            .for_each(|x| {
+                writeln!(cfile, "{}", x).unwrap();
+            });
+
+
+
+
 
         write_metrics(-1.0, end_seed - start_seed, entry_reciever.len() as i32).unwrap();
         thread::sleep(Duration::from_millis(100));
